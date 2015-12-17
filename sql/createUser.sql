caf77181c3803fd35481225b6dc93f3ab54da90a
Danny Fritz <dannyfritz@gmail.com>
2015-12-17 15:44:06 -0700

    :beginner: Commented SQL code

--- sql/createUser.sql
+++ sql/createUser.sql
@@ -1,2 +1,13 @@
 INSERT INTO "login" VALUES
   ($1, crypt($2, gen_salt('bf')));
+
+--NEVER STORE PASSWORDS AS PLAIN TEXT
+
+--crypt and gen_salt work together to
+--make the password not retrievable after
+--it is stored.
+
+--crypt and gen_salt are only available
+--using the pgcrypto module
+--enable with:
+--CREATE MODULE pgcrypto;
--- sql/getUser.sql
+++ sql/getUser.sql
@@ -2,3 +2,11 @@ SELECT username FROM "login"
 WHERE username=$1
   AND password_hash IS NOT NULL
   AND password_hash=crypt($2, password_hash);
+
+--PASSWORDS ARE NOT STORED AS PLAIN TEXT
+
+--Use crypt with the plain text password
+--provided by the user to see if the hash
+--matches the one stored
+
+--NEVER STORE PLAIN TEXT PASSWORDS
